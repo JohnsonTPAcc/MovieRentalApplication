8cd1ddb9d6ee424f57901e43b634c0f56efab82f
JohnsonTPAcc <116624569+JohnsonTPAcc@users.noreply.github.com>
2024-01-28 01:21:04 +0800

    Add project files.

---
+++ MovieRentalApplication/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MovieRentalApplication.Server.Models;
+
+namespace MovieRentalApplication.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
